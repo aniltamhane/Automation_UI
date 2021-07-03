f2cd47feda77a41aeab346c7124b178815a24dce
Anil <anil.tamhane@gmail.com>
2021-07-03 11:48:11 +0530

    Added compare string method

--- src/utils/StringUtils.java
+++ src/utils/StringUtils.java
@@ -9,5 +9,12 @@ public class StringUtils {
 		String s2 = s1.replaceAll("[$,]", "");
 		return Integer.parseInt(s2);
 	}
+	
+	//This method compares the strings
+	public boolean compareStrings(String s1, String s2){
+		
+		return s1.equals(s2);
+		
+	}
 
 }
