5996eb820c6ad7675940e6fd41f980c343628656
Anil <anil.tamhane@gmail.com>
2021-07-03 11:39:39 +0530

    This class handles string manipulation

---
+++ src/utils/StringUtils.java
@@ -0,0 +1,13 @@
+package utils;
+
+public class StringUtils {
+	
+	//Take argument in the format $34,500 and return int e.g. 34500
+	
+	public int getIntegerFromString(String s1){
+		
+		String s2 = s1.replaceAll("[$,]", "");
+		return Integer.parseInt(s2);
+	}
+
+}
